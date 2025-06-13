48b2011fa7b43cfa16cf9efd1d765e27c43d0ed9
shenxianpeng <xianpeng.shen@gmail.com>
2025-06-14 00:07:13 +0300

    fix: drop py39 and correct import package

--- src/badges/generator.py
+++ src/badges/generator.py
@@ -1,8 +1,8 @@
-import pybadges
+import badgepy
 
 
 def generate_badge(score: float, level: str, output: str):
-    svg = pybadges.badge(
+    svg = badgepy.badge(
         left_text="DevOps Maturity", right_text=level, right_color="green"
     )
     with open(output, "w") as f:
