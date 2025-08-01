de2eb27931ce3c1e78b32c605f73751805e3a831
shenxianpeng <xianpeng.shen@gmail.com>
2025-08-01 09:18:16 +0000

    chore: update WIP badge color to blue

--- src/core/badge.py
+++ src/core/badge.py
@@ -1,6 +1,6 @@
 def get_badge_url(level: str) -> str:
     BADGE_URLS = {
-        "WIP": "https://img.shields.io/badge/DevOps%20Maturity-WIP-red.svg",
+        "WIP": "https://img.shields.io/badge/DevOps%20Maturity-WIP-blue.svg",
         "PASSING": "https://img.shields.io/badge/DevOps%20Maturity-PASSING-green.svg",
         "BRONZE": "https://img.shields.io/badge/DevOps%20Maturity-BRONZE-yellow.svg",
         "SILVER": "https://img.shields.io/badge/DevOps%20Maturity-SILVER-silver.svg",
